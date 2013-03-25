06c1bc94ebc6ae20a9b7a111909a3615c586e8c4
prism <prism@hiren.(none)>
2013-03-25 09:44:12 +0600

    add march25 homework

---
+++ 2ndSem/25march-hw/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("No of lines : ");
+    scanf("%d",&a);
+    for(b=0;b<=a;b++)
+    {
+        for(c=0;c<=b;c++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
