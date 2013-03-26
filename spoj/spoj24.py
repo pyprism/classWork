36233ab4933d62f5031b40da0340b5a30b931a3c
pyprism <im not gonna give you>
2013-03-26 22:13:43 +0600

    update spoj

---
+++ spoj/spoj24.py
@@ -0,0 +1,24 @@
+def main():
+    while True:
+        line = int(input())
+        if 1<= line <= 100:
+            break
+        else:
+            pass
+    counter = 0
+    while (counter < line):
+        while True:
+            x = int(input())
+            if 1<= x <= 100:
+                break
+            else:pass
+        counterx = 1
+        result = 1
+        while(counterx <= x):
+            result = result * counterx
+            counterx = counterx + 1
+        counter = counter + 1
+        print(result)
+
+if __name__ =="__main__":
+    main()
